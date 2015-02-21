80a1025a85dab4c119d6d2124fe162cd388bde72
ncdesouza <nicholas.desouza@uoit.net>
2015-02-21 14:01:33 -0500

    modified: session.h - added Constructor and getter methods and setter prototypes.

--- xstream/src/data/session.h
+++ xstream/src/data/session.h
@@ -1,30 +1,38 @@
+#include <bits/stl_map.h>
+
 class Session
 {
+public:
+    Session() {
+    }
+
+    bool isLoggedIn_() const {
+        return loggedIn_;
+    }
+
+    void setLoggedIn_(bool loggedIn_) {
+        Session::loggedIn_ = loggedIn_;
+    }
+
+    string &getName_() const {
+        return name_;
+    }
+
+    void setName_(string &name_) {
+        Session::name_ = name_;
+    }
+
+    string &getType_() const {
+        return type_;
+    }
+
+    void setType_(string &type_) {
+        Session::type_ = type_;
+    }
 
 private:
     bool loggedIn_ = false;
     string name_;
     string type_;
-    std::map<int, Data()> transactions;
-
-public:
-    string getName() {
-        return name_;
-    }
-    void setName(string name);
-    
-    bool getloggedIn(){
-        return loggedIn_;
-    }
-
-    void setloggedIn(bool status);
-    
-    string getType(){
-        return type_;
-    }
-
-    void setType(char *type_);
-
-    void commit() {
-    }
+    std::map<int, Data()> transActions_;
 };
