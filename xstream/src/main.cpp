c3d0319c527b46dbaaf9773280ae9efb3d6f65d3
adminuser <adminuser@adminuser-VirtualBox.(none)>
2015-02-27 13:49:17 -0500

    test

--- xstream/src/main.cpp
+++ xstream/src/main.cpp
@@ -48,6 +48,7 @@ int main() {
                 break;
             case LOGOUT:
                 check = session.Logout();
+		break;
             case CREATE:
                 check = session.Create();
                 break;
