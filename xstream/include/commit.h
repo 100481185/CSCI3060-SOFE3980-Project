5ab39aac93d9bb9ab6eb12c904cda78b2177cc9f
ncdesouza <nicholas.desouza@uoit.net>
2015-03-01 02:05:09 -0500

    edited: moved constants to xstream.h

--- xstream/include/commit.h
+++ xstream/include/commit.h
@@ -1,26 +1,13 @@
 #ifndef XSTREAM_COMMIT_H
 #define XSTREAM_COMMIT_H
 
+#include "xstream.h"
 #include <sstream>
 #include <string.h>
 
 using namespace std;
 
-#define CODE_SIZE   2
-#define TYPE_SIZE   2
-#define NAME_SIZE   15
-#define CREDIT_SIZE 9
-#define EVENT_SIZE  19
-#define NUMTIC_SIZE 3
-#define PRICE_SIZE  6
 
-#define BLANK                   ' '
-#define CODE_FORMAT             "%02i"
-#define CREDIT_W_DEC_FORMAT     "%09.2f"
-#define CREDIT_WO_DEC_FORMAT    "%09i"
-#define NUMTIC_FORMAT           "%03i"
-#define PRICE_W_DEC_FORMAT      "%06.2f"
-#define PRICE_WO_DEC_FORMAT     "%06i"
 
 
 class Commit
