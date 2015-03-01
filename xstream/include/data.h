5f7c1051158301692a24e24a6a0a1d25a439e2cb
ncdesouza <nicholas.desouza@uoit.net>
2015-03-01 02:06:02 -0500

    edited: added comments

--- xstream/include/data.h
+++ xstream/include/data.h
@@ -6,42 +6,51 @@
 #include <string>
 #include <list>
 
+#define OPEN        "OpenFile(char flag): "
+#define CLOSE       "ClosrFile(): "
+#define READ        "ReadData(): "
+#define WRITE       "WriteData(): "
+
+#define ERROR       "error"
 
 using namespace std;
 
 class Data
 {
 public:
-
-    Data();
-
-    void set_file_name(string file_name);
+    /*
+    Constructor:
+        set file_name_
+     */
+    Data(string file_name);
 
     int ReadData();
 
     int WriteData();
 
-    void set_data(std::list<std::string> data);
-
-    std::list<std::string> data();
-
-//    std::list<std::string>::iterator data_ptr();
     std::list<std::string> data_;
 
 
 private:
-    // save file_name and assign a
-    // fstream variable.
+    // file_name_: the relative path to the file.
     std::string file_name_;
+    // *data_file_: the stream to read and write to the data file.
     std::fstream *data_file_;
 
-    // build data representation
-//    std::list<std::string> data_;
+    /*
+    OpenFile:
+        private method that handles the opening of files. It is
+        always called on read or write.
+        RETURN 0 on success, -1 on failure.
+     */
+    int OpenFile(char flag);
 
-    // Set open and
-    // close file methods to private
-    int OpenFile();
-
+    /*
+    CloseFile:
+        private method that handles the closing of files. Is is
+        always called on read or write.
+        RETURN 0 on success, -1 on failure.
+     */
     int CloseFile();
 };
 
