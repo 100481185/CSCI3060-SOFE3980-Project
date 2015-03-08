61ed194b19970a441b3f95c45ad8d7821da1b321
ncdesouza <nicholas.desouza@uoit.net>
2015-03-08 07:21:40 -0400

    modified TestFramework:
    fixed some dependency issues.

--- test/LoginTestSuite/ValidLoginTestCase/ValidLoginTestCase.sh
+++ test/LoginTestSuite/ValidLoginTestCase/ValidLoginTestCase.sh
@@ -8,5 +8,5 @@ function ValidLoginTestCase() {
 }
 
 
-
+setup
 ValidLoginTestCase
--- test/TestFramework/TestCase.sh
+++ test/TestFramework/TestCase.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 
-#exec 1> >(logger -s -t $(basename $0)) 2>&1
-
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # call if your test requires a clean build
 function setup {
-    echo ${DIR}/test_build
-    if [ -f ${DIR}/../test_build ]; then
-        rm -r ${DIR}/../test_build
-    fi
-
-    rm -r ${DIR}/../test_build
-    mkdir ${DIR}/../test_build && cd ${DIR}../test_build
+    clean
+    mkdir ${DIR}/../test_build && cd ${DIR}/../test_build
     cmake ${DIR}/../.. && make
 }
 
+function clean {
+    if [ -d ${DIR}/../test_build ]; then
+        rm -r ${DIR}/../test_build
+    fi
+}
 
 function TestCase {
-    setup
     cd ${DIR}/../test_build
-    ./xstream -s < $1/input.txt > $1/output.txt
+    ./xstream -s < $1/input.txt &> $1/output.txt
     diff $1/output.txt $1/expected.txt
     cp ${DIR}/../test_build/data $1/data -R
 }
