cf5e4d3c7e7f64786c4e52760f7b02cf13406977
ncdesouza <nicholas.desouza@uoit.net>
2015-03-14 15:23:55 -0400

    reverted directory changes

--- backend/src/xstream/Commit.java
+++
@@ -1,8 +0,0 @@
-package xstream;
-
-/**
- * This class represents a transaction. It is responsible
- * for storing and retrieving transaction data.
- */
-public class Commit {
-}
--- backend/src/xstream/User.java
+++
@@ -1,9 +0,0 @@
-package xstream;
-
-/**
- * This class represents an account for a User. It is
- * responsible for storing and retrieving a single users
- * information. Any access errors are reported to caller.
- */
-public class User {
-}
