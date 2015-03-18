5d00c10d661d9aa8e8849ecc8bbec009b8ae1cbf
ncdesouza <nicholas.desouza@uoit.net>
2015-03-18 18:13:47 -0400

    fixed problem with test constants.

--- backend/test/OperationManagerTest.java
+++ backend/test/OperationManagerTest.java
@@ -1,10 +1,9 @@
-import backend.Accounts;
-import backend.OperationManager;
-import backend.Tickets;
-import backend.Transactions;
+import backend.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -17,10 +16,10 @@ public class OperationManagerTest {
     private final String tr1Create = "01 user5           AA 999999999";
     private final String tr2Delete = "02 user5           AA 999999999";
     private final String tr3Sell = "03 event4               User1           050 050.25";
-    private final String tr4Buy = "04 event3               User3           003 050.25";
-    private final String tr5Return = "05 User1           User3           500.25";
-    private final String tr6AddCredit = "06 User1           AA 000050.20";
-    private final String tr0Logout = "00 User1           AA 999999999";
+    private final String tr4Buy = "04 event3               User4           000 000005";
+    private final String tr5Return = "05 user1           User4           00500";
+    private final String tr6AddCredit = "06 user1           AA 000050.20";
+    private final String tr0Logout = "00 user1           AA 999999999";
 
     // Accounts Constants
     private final String user1 = "user1           AA 999999999";
@@ -31,7 +30,7 @@ public class OperationManagerTest {
     // Tickets Constants
     private final String ticket1 = "event1               user1           100 000025";
     private final String ticket2 = "event2               user2           050 000.25";
-    private final String ticket3 = "event3               user4           050 000.25";
+    private final String ticket3 = "event3               user4           004 000005";
 
 
     private OperationManager opManager;
@@ -57,15 +56,54 @@ public class OperationManagerTest {
 
     @Test
     public void testProcessTransactions() throws Exception {
-        a
-
-        assertEquals(0, accounts.decode(""));
-
     }
 
     @Test
     public void testSetLoggedIn() throws Exception {
+        // create a transaction of a create and a logout
+        transactions.decode(tr1Create);
+        transactions.decode(tr0Logout);
+        // create a user account
+        assertEquals(0, accounts.decode(user1));
 
+// Experienced trouble getting the currently logged in user.
+// Discovered test strategies to access private attributes and methods using
+// java.lang.reflect library. Problem solved.
+///////////////////////////////////////////////////////////////////////////////////
+//        Field records = OperationManager.class.getDeclaredField("transactions");
+//        records.setAccessible(true);
+//
+//        Transactions cur = (Transactions) records.get(opManager);
+//        System.out.println(cur.getLoggedInUser());
+//        cur = (Transactions) records.get(opManager);
+//        System.out.println(cur.getLoggedInUser());
+//        Field accounts = OperationManager.class.getDeclaredField("accounts");
+//        accounts.setAccessible(true);
+//
+//        Accounts users = (Accounts) accounts.get(opManager);
+//        User curUser = users.getUser("user1");
+//        TestCase.assertNotNull(curUser);
+//        System.out.println(curUser.getName());
+///////////////////////////////////////////////////////////////////////////////////
+
+        User user = opManager.setLoggedIn();
+        System.out.println(user.getName());
+        assertEquals("user1", user.getName());
+    }
+
+    @Test
+    public void testDoCreate() throws Exception {
+        // create a transaction of a create and a logout
+        transactions.decode(tr1Create);
+        transactions.decode(tr0Logout);
+        // create a user account
+        accounts.decode(user1);
+
+        Method doCreate = OperationManager.class.getDeclaredMethod("doCreate", Regular.class);
+        doCreate.setAccessible(true);
+        Object ret = doCreate.invoke(opManager, transactions.getTransaction());
+        System.out.print(ret);
+        assertEquals(0, ret);
     }
 
     @Test
